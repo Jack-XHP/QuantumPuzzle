6cd42f215cd8d11aa33adb1df98fbc01d55bc1b8
Jack-XHP <haoping.xu@mail.utoronto.ca>
2019-10-30 20:36:31 -0400

    add mutli level numberlink support

--- numberLink.py
+++ numberLink.py
@@ -70,25 +70,35 @@ def sumToN2(neighbor, target, J, scale=1):
                 J[term] = weight * scale
 
 
-def oneLayer(origin, J,J_3D, size, layer):
-    hight, width = size
-    grid = np.arange((layer-1)*hight * width, layer*hight * width).reshape((hight, width))
+def sumLessOne(neighbor, J, scale=1):
+    for ele1 in neighbor:
+        for ele2 in neighbor:
+            term = (ele1, ele2)
+            if ele1 < ele2:
+                if term in J:
+                    J[term] += scale
+                else:
+                    J[term] = scale
+
+
+def oneLayer(origin, J,J_3D, grid):
+    hight, width = grid.shape
     origin_var = [grid[index] for index in origin]
-    all_var = [i for i in range(0, hight * width)]
+    all_var = grid.flatten()
     min_path_len = abs(origin[0][0] - origin[1][0]) + abs(origin[0][1] - origin[1][1])
-    sumToN2(all_var, min_path_len, J)
-    for x in range(0, hight * width):
+    sumToN2(all_var, min_path_len, J, scale=1.2)
+    for x in all_var:
         term = (x, x)
         if x in origin_var:
-            weight = -10
+            weight = -hight*width * 1000
         else:
-            weight = 1
+            weight = 0
         if term in J:
             J[term] += weight
         else:
             J[term] = weight
-        i = x // width
-        j = x % width
+        i = (x-grid[0,0]) // width
+        j = (x-grid[0,0]) % width
         near = []
         if j > 0:
             neighbor = grid[i][j - 1]
@@ -103,9 +113,9 @@ def oneLayer(origin, J,J_3D, size, layer):
             neighbor = grid[i + 1][j]
             near.append(neighbor)
         if x in origin_var:
-            sumToN(x, near, 1, J,J_3D, scale=5)
+            sumToN(x, near, 1, J,J_3D, scale=10)
         else:
-            sumToN(x, near, 2, J,J_3D)
+            sumToN(x, near, 2, J,J_3D, scale=1)
 
 
 # 2 degree term
@@ -113,29 +123,56 @@ J = {}
 # 3 degree term
 J_3D = {}
 
-hight, width = (4,4)
+depth, hight, width = (2,6,6)
+grid = np.arange(depth*hight*width).reshape((depth, hight, width))
 
-origin = [(0,0), (3,3)]
-oneLayer(origin, J, J_3D, (hight,width), 1)
+origin = (
+    ((0,0), (5,5)),
+    ((1,3), (3,3)),
+)
+origin_list = [i for i in origin]
+origin_list = [i for i in origin_list]
+for i in range(depth):
+    oneLayer(origin[i], J, J_3D, grid[i])
+
+if depth > 1:
+    for x in range(hight):
+        for y in range(width):
+            same_bis = grid[:, x, y].flatten()
+            if (x, y) in origin_list:
+                scale = hight * width * 1000
+            else:
+                scale = 6
+            sumLessOne(same_bis, J, scale=scale)
+
 
 # convert all 3 degree term to qubo form
 reduceBySubstitution(J, J_3D, 2)
 
 
 print(J)
-res = QBSolv().sample_qubo(J)
+res = QBSolv().sample_qubo(J, num_repeats=1000)
 samples = list(res.samples())
 energy = list(res.data_vectors['energy'])
 print(samples)
 print(energy)
 for i in range(len(samples)):
     result = samples[i]
-    output = []
+    output = np.zeros((hight, width))
     # ignore ancillary variables, which are all negative, only get positive bits
-    for x in range(0, hight * width):
-        output.append(result[x])
-    output = np.array(output).reshape((hight, width))
-    for ori in origin:
-        output[ori] = -1
+    for l in range(depth):
+        for x in range(hight):
+            for y in range(width):
+                bit = result[grid[l, x, y]] * (l+1)
+                if bit != 0:
+                    old_state = output[x, y]
+                    if old_state != 0:
+                        output[x,y] = old_state * 10 + bit
+                    else:
+                        output[x, y] = bit
+    for l in range(depth):
+        for ori in origin[l]:
+            old_state = output[ori]
+            output[ori] = old_state + 0.1 * (l+1)
     print("energy: {}_____________________________".format(energy[i]))
     print(output)

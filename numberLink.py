b97f6e6ecb728ef01e01b5e79687146bee7a3929
Jack-XHP <haoping.xu@mail.utoronto.ca>
2019-11-12 15:48:42 -0500

    update numberlink to run on qpu and fix bug about min distance between origin and one number in orgin across layers; run some puzzle configs solved by simulator and same config run on qpu

--- numberLink.py
+++ numberLink.py
@@ -1,5 +1,10 @@
 from dwave_qbsolv import QBSolv
 import numpy as np
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import FixedEmbeddingComposite
+import minorminer
+import networkx as nx
+import numpy as np
 
 # xyz =min_w wx + M(yz - 2(y+z)w + 3w)
 # https://docs.dwavesys.com/docs/latest/c_handbook_3.html#reduction-by-substitution
@@ -51,7 +56,7 @@ def sumToN(center, neighbor, target, J, J_3D, scale=1):
                         J_3D[term] = weight
 
 
-#scale*(sum(neighbor) - target)^2
+# scale*(sum(neighbor) - target)^2
 def sumToN2(neighbor, target, J, scale=1):
     for ele1 in neighbor:
         for ele2 in neighbor:
@@ -70,6 +75,7 @@ def sumToN2(neighbor, target, J, scale=1):
                 J[term] = weight * scale
 
 
+# sum xi * xj, where xi in neighbor and i != j, has minimum when at most one xi = 0
 def sumLessOne(neighbor, J, scale=1):
     for ele1 in neighbor:
         for ele2 in neighbor:
@@ -84,19 +90,26 @@ def sumLessOne(neighbor, J, scale=1):
 def oneLayer(origin, J,J_3D, grid):
     hight, width = grid.shape
     origin_var = [grid[index] for index in origin]
+    origin = np.array(origin)
+
+    # let one layer's qubits to sum up to min distance between two origins
     all_var = grid.flatten()
-    min_path_len = abs(origin[0][0] - origin[1][0]) + abs(origin[0][1] - origin[1][1])
-    sumToN2(all_var, min_path_len, J, scale=1.2)
+    min_path_len = abs(origin[0][0] - origin[1][0]) + abs(origin[0][1] - origin[1][1]) + 1
+    sumToN2(all_var, min_path_len, J, scale=w_minlen)
+
     for x in all_var:
+        # make sure origin has a number and discourage other cell to have number
         term = (x, x)
         if x in origin_var:
-            weight = -hight*width * 1000
+            weight = w_ori
         else:
-            weight = 0
+            weight = w_each
         if term in J:
             J[term] += weight
         else:
             J[term] = weight
+
+        # determine cell's neighbors
         i = (x-grid[0,0]) // width
         j = (x-grid[0,0]) % width
         near = []
@@ -112,67 +125,95 @@ def oneLayer(origin, J,J_3D, grid):
         if i < hight - 1:
             neighbor = grid[i + 1][j]
             near.append(neighbor)
+
+        # allow 2 numbered neighbor for each cell with number
         if x in origin_var:
-            sumToN(x, near, 1, J,J_3D, scale=10)
+            # in case of origin, only one neighbor can be numbered
+            sumToN(x, near, 1, J,J_3D, scale=w_orinear)
         else:
-            sumToN(x, near, 2, J,J_3D, scale=1)
+            #distance = origin - [i, j]
+            #minD2Ori = np.min(np.sum(np.abs(distance), axis=1))
+            sumToN(x, near, 2, J,J_3D, scale=w_near)
 
 
-# 2 degree term
-J = {}
-# 3 degree term
-J_3D = {}
+if __name__ == "__main__":
+    # 2 degree term
+    J = {}
+    # 3 degree term
+    J_3D = {}
 
-depth, hight, width = (2,6,6)
-grid = np.arange(depth*hight*width).reshape((depth, hight, width))
+    depth, hight, width = (3, 4, 4)
+    grid = np.arange(depth * hight * width).reshape((depth, hight, width))
+    w_ori = -hight * width * 1000
+    w_each = 0
+    w_minlen = 1
+    w_orinear = 100
+    w_near = 1
+    w_same = 100
+    w_sameori = hight * width * 1000
+    origin = (
+        ((0, 0), (3, 3)),
+        ((0, 1), (2, 1)),
+        ((0, 2), (2, 2)),
+    )
+    use_qpu = True
+    # collect all params used to define puzzle, qubo weights and solver type
+    params = ["w_ori", "w_each", "w_minlen", "w_orinear", "w_near", "w_same", "w_sameori", "origin", "use_qpu"]
+    params = {k: eval(k) for k in params}
+    print(params)
 
-origin = (
-    ((0,0), (5,5)),
-    ((1,3), (3,3)),
-)
-origin_list = [i for i in origin]
-origin_list = [i for i in origin_list]
-for i in range(depth):
-    oneLayer(origin[i], J, J_3D, grid[i])
+    origin_list = [i[0] for i in origin] + [i[1] for i in origin]
 
-if depth > 1:
-    for x in range(hight):
-        for y in range(width):
-            same_bis = grid[:, x, y].flatten()
-            if (x, y) in origin_list:
-                scale = hight * width * 1000
-            else:
-                scale = 6
-            sumLessOne(same_bis, J, scale=scale)
+    # each layer conditions
+    for i in range(depth):
+        oneLayer(origin[i], J, J_3D, grid[i])
 
-
-# convert all 3 degree term to qubo form
-reduceBySubstitution(J, J_3D, 2)
-
-
-print(J)
-res = QBSolv().sample_qubo(J, num_repeats=1000)
-samples = list(res.samples())
-energy = list(res.data_vectors['energy'])
-print(samples)
-print(energy)
-for i in range(len(samples)):
-    result = samples[i]
-    output = np.zeros((hight, width))
-    # ignore ancillary variables, which are all negative, only get positive bits
-    for l in range(depth):
+    # one number in one cell condition
+    if depth > 1:
         for x in range(hight):
             for y in range(width):
-                bit = result[grid[l, x, y]] * (l+1)
-                if bit != 0:
-                    old_state = output[x, y]
-                    if old_state != 0:
-                        output[x,y] = old_state * 10 + bit
-                    else:
-                        output[x, y] = bit
-    for l in range(depth):
-        for ori in origin[l]:
-            old_state = output[ori]
-            output[ori] = old_state + 0.1 * (l+1)
-    print("energy: {}_____________________________".format(energy[i]))
-    print(output)
+                same_bis = grid[:, x, y].flatten()
+                if (x, y) in origin_list:
+                    scale = w_sameori
+                else:
+                    scale = w_same
+                sumLessOne(same_bis, J, scale=scale)
+
+    # convert all 3 degree term to qubo form
+    reduceBySubstitution(J, J_3D, 2)
+    # show QUBO embedding
+    print(J)
+
+    if use_qpu:
+        solver_limit = 50
+        G = nx.complete_graph(solver_limit)
+        system = DWaveSampler()
+        embedding = minorminer.find_embedding(G.edges, system.edgelist)
+        print(embedding)
+        res = QBSolv().sample_qubo(J, solver=FixedEmbeddingComposite(system, embedding), solver_limit=solver_limit)
+    else:
+        res = QBSolv().sample_qubo(J, num_repeats=1000)
+    samples = list(res.samples())
+    energy = list(res.data_vectors['energy'])
+    print(samples)
+    print(energy)
+    for i in range(len(samples)):
+        result = samples[i]
+        output = np.zeros((hight, width))
+        # ignore ancillary variables, which are all negative, only get positive bits
+        for l in range(depth):
+            for x in range(hight):
+                for y in range(width):
+                    bit = result[grid[l, x, y]] * (l + 1)
+                    if bit != 0:
+                        old_state = output[x, y]
+                        if old_state != 0:
+                            output[x, y] = old_state * 10 + bit
+                        else:
+                            output[x, y] = bit
+        for l in range(depth):
+            for ori in origin[l]:
+                old_state = output[ori]
+                output[ori] = old_state + 0.1 * (l + 1)
+        print("energy: {}_____________________________".format(energy[i]))
+        print(output)
